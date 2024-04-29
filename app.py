2c13bcc5020a524467da41a2af9659c166707313
Eric Li <cesir@foxmail.com>
2024-04-29 15:56:45 +0800

    Initial

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template, redirect, url_for
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return "No file part"
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return "No selected file"
+
+    if file:
+        df = pd.read_csv(file)
+        process(df)
+        return redirect(url_for('upload_file'))  # redirect to main page
+
+
+def process(df):
+    print(df.head())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
